e5d34698ba63f4c078c793a2c1e819c1adc9a8b2
chung-ejy <chung.ejy@gmail.com>
2023-05-16 06:55:58 +0900

    added security

--- portfolio_api/datacruncher/datacruncher.py
+++ portfolio_api/datacruncher/datacruncher.py
@@ -88,31 +88,34 @@ class Datacruncher(object):
         try:
             artist_name = data["artist_name"]
             track_name = data["track_name"]
-            spotify = Spotify()
-            spotify.connect()
-            current = spotify.find_song_uri(artist_name,track_name).iloc[0]
-            spotify.disconnect()
-            current_pid = current["pid"]
-            uri = current["track_uri"]
-            spotify.connect()
-            included_playlists = spotify.find_included_playlists(uri)
-            pids = included_playlists["pid"].unique()
-            spotify.disconnect()
-            aggregate = []
-            spotify.connect()
-            for pid in pids:
-                if pid != current_pid:
-                    songs = spotify.find_playlist_songs(int(pid))
-                    aggregate.append(songs)
-            spotify.disconnect()
-            s = pd.concat(aggregate)
-            max_follower = s["num_holdouts"].max()
-            s["follower_percentage"] = s["num_holdouts"] / max_follower
-            s["count"] = 1 * s["follower_percentage"]
-            analysis = s.groupby(["track_uri","artist_uri","artist_name","track_name"]).sum().reset_index()
-            recs = analysis.sort_values("count",ascending=False)
-            rec = recs[(recs["track_name"] != track_name)].sort_values("count",ascending=False).iloc[1]
-            complete = {"artist_name":artist_name,"track_name":track_name,"artist_rec":rec["artist_name"],"track_rec":rec["track_name"]}
+            if len(artist_name) < 30 and len(track_name) < 30:
+                spotify = Spotify()
+                spotify.connect()
+                current = spotify.find_song_uri(artist_name,track_name).iloc[0]
+                spotify.disconnect()
+                current_pid = current["pid"]
+                uri = current["track_uri"]
+                spotify.connect()
+                included_playlists = spotify.find_included_playlists(uri)
+                pids = included_playlists["pid"].unique()
+                spotify.disconnect()
+                aggregate = []
+                spotify.connect()
+                for pid in pids:
+                    if pid != current_pid:
+                        songs = spotify.find_playlist_songs(int(pid))
+                        aggregate.append(songs)
+                spotify.disconnect()
+                s = pd.concat(aggregate)
+                max_follower = s["num_holdouts"].max()
+                s["follower_percentage"] = s["num_holdouts"] / max_follower
+                s["count"] = 1 * s["follower_percentage"]
+                analysis = s.groupby(["track_uri","artist_uri","artist_name","track_name"]).sum().reset_index()
+                recs = analysis.sort_values("count",ascending=False)
+                rec = recs[(recs["track_name"] != track_name)].sort_values("count",ascending=False).iloc[1]
+                complete = {"artist_name":artist_name,"track_name":track_name,"artist_rec":rec["artist_name"],"track_rec":rec["track_name"]}
+            else:
+                complete = {"artist_name":artist_name,"track_name":track_name,"artist_rec":"none found","track_rec":"none found","error":str(e)}    
         except Exception as e:
             complete = {"artist_name":artist_name,"track_name":track_name,"artist_rec":"none found","track_rec":"none found","error":str(e)}
         return complete
