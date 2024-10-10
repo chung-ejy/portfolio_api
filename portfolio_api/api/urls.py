6d01b1c45e548fb8e6836e7d6c2be531f85b1b39
chung-ejy <chung.ejy@gmail.com>
2024-10-10 05:43:25 -0400

    added slashes to route names

--- portfolio_api/api/urls.py
+++ portfolio_api/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("",views.apiView,name="api"),
-    path("visualization",views.visualizationView,name="visualization"),
-    path("trades",views.tradesView,name="trades")
+    path("/visualization",views.visualizationView,name="visualization"),
+    path("/trades",views.tradesView,name="trades")
 ]
